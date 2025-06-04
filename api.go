80ce0340c4941e2dc2b6ff53cc7f100e3e9115e0
Rohit Sarkar <rohitsarkar034@gmail.com>
2025-06-04 23:20:41 +0530

    update: handleAccount and handleCreateAccount function to act as per different request types

--- api.go
+++ api.go
@@ -9,34 +9,15 @@ import (
 	"github.com/gorilla/mux"
 )
 
-func WriteJson(w http.ResponseWriter, status int, v any) error {
-	w.WriteHeader(status)
-	w.Header().Set("Content-Type", "application/json")
-	return json.NewEncoder(w).Encode(v)
-}
-
-type apiFunc func (http.ResponseWriter, *http.Request) error 
-
-type apiError struct {
-	Error string
-}
-	
-func makeHttpHandleFunc (f apiFunc) http.HandlerFunc {
-	return func (w http.ResponseWriter, r *http.Request)  {
-		if err := f(w, r); err != nil {
-			//handle error
-			WriteJson(w, http.StatusBadRequest, apiError{Error: err.Error()})
-		}
-	}
-}
-
 type ApiServer struct {
 	listenAddr string
+	store      Storage
 }
 
-func NewAPIServer(listenAddr string) *ApiServer {
+func NewAPIServer(listenAddr string, store Storage) *ApiServer {
 	return &ApiServer{
 		listenAddr: listenAddr,
+		store:      store,
 	}
 }
 
@@ -51,13 +32,13 @@ func (s *ApiServer) Run() {
 }
 
 func (s *ApiServer) handleAccount(w http.ResponseWriter, r *http.Request) error {
-	if r.Method == "GET"{
+	if r.Method == "GET" {
 		return s.handleGetAccount(w, r)
 	}
-	if r.Method == "POST"{
+	if r.Method == "POST" {
 		return s.handleCreateAccount(w, r)
 	}
-	if r.Method == "DELETE"{
+	if r.Method == "DELETE" {
 		return s.handleDeleteAccount(w, r)
 	}
 	return fmt.Errorf("method not allowed %s", r.Method)
@@ -71,7 +52,16 @@ func (s *ApiServer) handleGetAccount(w http.ResponseWriter, r *http.Request) err
 }
 
 func (s *ApiServer) handleCreateAccount(w http.ResponseWriter, r *http.Request) error {
-	return nil
+	createAccountReq := new(CreateAccountRequest)
+	if err := json.NewDecoder(r.Body).Decode(createAccountReq); err != nil {
+		return err
+	}
+	account := NewAccount(createAccountReq.FirstName, createAccountReq.LastName)
+	if err := s.store.CreateAccount(account); err != nil {
+		return err
+	}
+
+	return WriteJson(w, http.StatusOK, account)
 }
 
 func (s *ApiServer) handleDeleteAccount(w http.ResponseWriter, r *http.Request) error {
@@ -82,3 +72,23 @@ func (s *ApiServer) handleTransfer(w http.ResponseWriter, r *http.Request) error
 	return nil
 }
 
+func WriteJson(w http.ResponseWriter, status int, v any) error {
+	w.Header().Add("Content-Type", "application/json")
+	w.WriteHeader(status)
+	return json.NewEncoder(w).Encode(v)
+}
+
+type apiFunc func(http.ResponseWriter, *http.Request) error
+
+type apiError struct {
+	Error string
+}
+
+func makeHttpHandleFunc(f apiFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		if err := f(w, r); err != nil {
+			//handle error
+			WriteJson(w, http.StatusBadRequest, apiError{Error: err.Error()})
+		}
+	}
+}
