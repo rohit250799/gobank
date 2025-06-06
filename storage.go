6c0d1495c1c6c709a708c2b0537a782a74e20d0a
Rohit Sarkar <rohitsarkar034@gmail.com>
2025-06-06 08:06:33 +0530

    modifying functions to fetch accounts by id and delete them

--- storage.go
+++ storage.go
@@ -12,7 +12,7 @@ import (
 
 type Storage interface {
 	CreateAccount(*Account) error
-	DeleteAccount(*Account) error
+	DeleteAccount(int) error
 	UpdateAccount(*Account) error
 	GetAccounts() ([]*Account, error)
 	GetAccountById(int) 	(*Account, error)
@@ -78,12 +78,22 @@ func (s *PostgresStore) UpdateAccount (*Account) error {
 	return nil
 }
 
-func (s *PostgresStore) DeleteAccount (*Account) error {
-	return nil	
+func (s *PostgresStore) DeleteAccount (id int) error {
+	_, err := s.db.Query(`delete from account where id = $1`, id)
+	return err	
 }
 
 func (s *PostgresStore) GetAccountById (id int) (*Account, error) {
-	return nil, nil
+	rows, err := s.db.Query(`select * from account where id = $1`, id)
+	if err != nil {
+		return nil, err
+	}
+
+	for rows.Next() {
+		return scanIntoAccount(rows)
+	}
+	
+	return nil, fmt.Errorf("account %d not found", id)
 }
 
 func (s *PostgresStore) GetAccounts() ([]*Account, error) {
@@ -95,15 +105,7 @@ func (s *PostgresStore) GetAccounts() ([]*Account, error) {
 
 	accounts := []*Account{}
 	for rows.Next() {
-		account := new(Account)
-		err := rows.Scan(
-			&account.ID, 
-			&account.FirstName, 
-			&account.LastName, 
-			&account.Number, 
-			&account.Balance, 
-			&account.CreatedAt);
-
+		account, err := scanIntoAccount(rows)
 
 		if err != nil {
 			return nil, err
@@ -113,4 +115,18 @@ func (s *PostgresStore) GetAccounts() ([]*Account, error) {
 	}
 
 	return accounts, nil
+}
+
+func scanIntoAccount(rows *sql.Rows) (*Account, error) {
+	account := new(Account)
+	err := rows.Scan(
+		&account.ID, 
+		&account.FirstName, 
+		&account.LastName, 
+		&account.Number, 
+		&account.Balance, 
+		&account.CreatedAt);
+
+	return account, err	
+
 }
